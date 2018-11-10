ca6cd763d60dff324a49b1e282b9d732e4137e47
Justin Davis <JustinDavisA@Gmail.com>
2018-11-09 22:17:36 -0500

    Added scripts and Scene

---
+++ PortalTextureSetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+    public Camera cameraDarkWorld;
+    public Camera cameraLightWorld;
+
+    public Material cameraMaterialDarkWorld;
+    public Material cameraMaterialLightWorld;
+	
+	// Use this fo initialization
+	void Start ()
+    {
+        if(cameraLightWorld.targetTexture != null)
+        {
+            cameraLightWorld.targetTexture.Release();
+        }
+        cameraLightWorld.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMaterialLightWorld.mainTexture = cameraLightWorld.targetTexture;
+
+        if (cameraDarkWorld.targetTexture != null)
+        {
+            cameraDarkWorld.targetTexture.Release();
+        }
+        cameraDarkWorld.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMaterialDarkWorld.mainTexture = cameraDarkWorld.targetTexture;
+
+	}
+}
